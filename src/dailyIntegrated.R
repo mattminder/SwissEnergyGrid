94a4f21cd7b725a1bfdbbfd07e096aa1b30e7060
mattminder <myfiles@ext-139-97.eduroam.unibe.ch>
2019-06-03 12:39:38 +0200

    Fixed singularity issue

--- src/dailyIntegrated.R
+++ src/dailyIntegrated.R
@@ -77,9 +77,7 @@ dailyT = apply.daily(energyTS$Temperature, mean)[-1]
 dailyT = remove_leap_days(dailyT)
 
 
-
-# --------------------- MODEL 1 ---------------------------------
-# ---------- regress out weekly, yearly and holidays ------------
+# -------------------- sine regressors ---------------------------
 weekPer = 7
 yearPer = 365
 t = 1:length(dailyInt)
@@ -97,10 +95,14 @@ cy4 <- cos(2*pi*t*4/yearPer)
 sy5 <- sin(2*pi*t*5/yearPer)
 cy5 <- cos(2*pi*t*5/yearPer)
 
-mod1 <- arima(dailyT, order=c(1, 1, 3), 
+
+# --------------------- MODEL 1 ---------------------------------
+# ---------- regress out weekly, yearly and holidays ------------
+
+mod1 <- arima(dailyInt/100, order=c(1, 1, 3), 
               seasonal = list(order=c(2, 1, 1), period=7),
-              xreg=cbind(saturdayIx, sundayIx, dailyT))
-                         #sy1,cy1,sy2,cy2))#,sy3,cy3,sy4,cy4,sy5,cy5))
+              xreg=cbind(saturdayIx, sundayIx, dailyT))#,
+                        #sy1,cy1,sy2,cy2,sy3,cy3,sy4,cy4,sy5,cy5))
 tsdiag(mod1)
 
 
@@ -110,10 +112,10 @@ qqline(mod1$residuals)
 
 
 # Only regressors
-mod1 <- arima(dailyT, order=c(1, 1, 3), 
-              seasonal = list(order=c(2, 1, 1), period=7),
-              xreg=cbind(saturdayIx, sundayIx, 
-                         sy1,cy1,sy2,cy2))#,sy3,cy3,sy4,cy4,sy5,cy5))
+# mod1 <- arima(dailyInt, order=c(1, 1, 3),
+#               seasonal = list(order=c(2, 1, 1), period=7),
+#               xreg=cbind(saturdayIx, sundayIx,
+#                          sy1,cy1,sy2,cy2))#,sy3,cy3,sy4,cy4,sy5,cy5))
 
 
 
@@ -155,35 +157,35 @@ mod1 <- arima(dailyT, order=c(1, 1, 3),
 # 
 # res_decomp = stl(res1, "p", robust = T)
 # plot(res_decomp)
-
-
-# --------------------------- sarima -------------------------------------
-mod1 <- sarima(res1, 1, 1, 3, 2, 1, 1, 7)
-
-
-
-# --------------------------- model 2 -------------------------------------
-# treat saturday and sundays seperately
-# ---------- regress out weekly, yearly and holidays ------------
-weekPer = 7
-yearPer = 365
-t = 1:length(dailyInt)
-reg2 <- lm(dailyInt ~ saturdayIx + sundayIx + 
-             sin(2*pi*t/weekPer)+cos(2*pi*t/weekPer) +
-             # sin(2*pi*t*2/weekPer) + cos(2*pi*t*2/weekPer) +
-             # sin(2*pi*t*3/weekPer) + cos(2*pi*t*3/weekPer) +
-             sin(2*pi*t/yearPer)+cos(2*pi*t/yearPer) +
-             sin(2*pi*t*2/yearPer)+cos(2*pi*t*2/yearPer) +
-             sin(2*pi*t*3/yearPer)+cos(2*pi*t*3/yearPer) +
-             sin(2*pi*t*4/yearPer)+cos(2*pi*t*4/yearPer) +
-             sin(2*pi*t*5/yearPer)+cos(2*pi*t*5/yearPer) +
-             dailyT)
-
-
-
-res2 <- dailyInt - fitted(reg2)
-
-plot(res2)
-plot(diff(res2))
-Acf(diff(res2))
-
+# 
+# 
+# # --------------------------- sarima -------------------------------------
+# mod1 <- sarima(res1, 1, 1, 3, 2, 1, 1, 7)
+# 
+# 
+# 
+# # --------------------------- model 2 -------------------------------------
+# # treat saturday and sundays seperately
+# # ---------- regress out weekly, yearly and holidays ------------
+# weekPer = 7
+# yearPer = 365
+# t = 1:length(dailyInt)
+# reg2 <- lm(dailyInt ~ saturdayIx + sundayIx + 
+#              sin(2*pi*t/weekPer)+cos(2*pi*t/weekPer) +
+#              # sin(2*pi*t*2/weekPer) + cos(2*pi*t*2/weekPer) +
+#              # sin(2*pi*t*3/weekPer) + cos(2*pi*t*3/weekPer) +
+#              sin(2*pi*t/yearPer)+cos(2*pi*t/yearPer) +
+#              sin(2*pi*t*2/yearPer)+cos(2*pi*t*2/yearPer) +
+#              sin(2*pi*t*3/yearPer)+cos(2*pi*t*3/yearPer) +
+#              sin(2*pi*t*4/yearPer)+cos(2*pi*t*4/yearPer) +
+#              sin(2*pi*t*5/yearPer)+cos(2*pi*t*5/yearPer) +
+#              dailyT)
+# 
+# 
+# 
+# res2 <- dailyInt - fitted(reg2)
+# 
+# plot(res2)
+# plot(diff(res2))
+# Acf(diff(res2))
+# 
