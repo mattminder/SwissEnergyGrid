179a7bfd96451f776944a7792fac6fec0926dfe1
mattminder <myfiles@ext-139-97.eduroam.unibe.ch>
2019-06-03 09:40:04 +0200

    updated daily

--- src/dailyIntegrated.R
+++ src/dailyIntegrated.R
@@ -4,6 +4,7 @@ library(xts)
 library(dplyr)
 library(forecast)
 library(astsa)
+library(tis)
 # ------------------------- load the data-------------------------------
 # Set path
 homePath = "/Users/myfiles/Documents/EPFL/M_II/TSE/SwissEnergyGrid/"
@@ -27,17 +28,47 @@ dailyInt = remove_leap_days(dailyInt)
 attr(dailyInt, 'frequency') <- 365
 
 
-# ----------------- define holiday season -----------------------
+# ----------------- define holidays -----------------------
 # Holiday season between 24.12 and 2.1
+# + August first, easter monday, good friday, pentecoste
+# ascension
 ix = index(dailyInt)
 day = as.numeric(format(ix, "%d"))
 month = as.numeric(format(ix, "%m"))
 year = as.numeric(format(ix, "%y"))
 
+ndays = length(ix)
+
 holidayIx = (day >= 24 & month == 12) | (day <= 2 & month == 1)
 weekendIx = ix$wday %in% c(0,6)
 
-# -------------------- temperature 
+easterIx = isEaster(ix)
+whereEaster = which(easterIx)
+
+goodfridayIx = rep(F, ndays)
+goodfridayIx[whereEaster - 2] = T
+
+easterMondayIx = rep(F, ndays)
+easterMondayIx[whereEaster + 1] = T
+
+pentecostIx = rep(F, ndays)
+pentecostIx[whereEaster + 50] = T
+
+ascensionIx = rep(F, ndays)
+ascensionIx[whereEaster + 39] = T
+
+ascensionFridayIx = rep(F, ndays)
+ascensionFridayIx[whereEaster + 40] = T
+
+firstOfAugustIx = (day == 1 & month == 8)
+
+holidayIx = holidayIx | goodfridayIx | pentecostIx  | 
+  ascensionIx | firstOfAugustIx | easterMondayIx | ascensionFridayIx
+
+# -------------------- temperature ------------------------------
+dailyT = apply.daily(energyTS$Temperature, mean)[-1]
+dailyT = remove_leap_days(dailyT)
+
 
 
 # --------------------- MODEL 1 ---------------------------------
@@ -46,13 +77,15 @@ weekPer = 7
 yearPer = 365
 t = 1:length(dailyInt)
 reg1 <- lm(dailyInt ~ holidayIx + weekendIx +
-             sin(2*pi*t/weekPer)+cos(2*pi*t/weekPer) +
-             sin(2*pi*t*2/weekPer) + cos(2*pi*t*2/weekPer) +
+             # sin(2*pi*t/weekPer)+cos(2*pi*t/weekPer) +
+             # sin(2*pi*t*2/weekPer) + cos(2*pi*t*2/weekPer) +
              # sin(2*pi*t*3/weekPer) + cos(2*pi*t*3/weekPer) +
-             sin(2*pi*t/yearPer)+cos(2*pi*t/yearPer) + 
-             sin(2*pi*t*2/yearPer)+cos(2*pi*t*2/yearPer) + 
-             sin(2*pi*t*3/yearPer)+cos(2*pi*t*3/yearPer) + 
-             sin(2*pi*t*4/yearPer)+cos(2*pi*t*4/yearPer))
+             sin(2*pi*t/yearPer)+cos(2*pi*t/yearPer) +
+             sin(2*pi*t*2/yearPer)+cos(2*pi*t*2/yearPer) +
+             sin(2*pi*t*3/yearPer)+cos(2*pi*t*3/yearPer) +
+             sin(2*pi*t*4/yearPer)+cos(2*pi*t*4/yearPer) +
+             sin(2*pi*t*5/yearPer)+cos(2*pi*t*5/yearPer) +
+             dailyT)
 
 
 
@@ -72,6 +105,12 @@ par(mfrow=c(1,1))
 
 res_decomp = stl(res1, "p", robust = T)
 plot(res_decomp)
+
+
+# --------------------------- sarima -------------------------------------
+mod1 <- sarima(res1, 1, 1, 5, 1, 1, 1, 7)
+
+
 # ---------------------- sarima grid search ------------------------------
 # aic_grid = list()
 # aic_param = list()
