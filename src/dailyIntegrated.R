257aa0236f9f2677da0dd644d660072cf5ade122
mattminder <myfiles@Mattus-MacBook-Pro.local>
2019-06-04 10:10:40 +0200

    changed report, daily integrated

--- src/dailyIntegrated.R
+++ src/dailyIntegrated.R
@@ -84,6 +84,9 @@ t = 1:length(dailyInt)
 
 sw1 <- sin(2*pi*t/weekPer)
 cw1 <- cos(2*pi*t/weekPer)
+sw2 <- sin(2*pi*t*2/weekPer)
+cw2 <- cos(2*pi*t*2/weekPer)
+
 sy1 <- sin(2*pi*t/yearPer)
 cy1 <- cos(2*pi*t/yearPer)
 sy2 <- sin(2*pi*t*2/yearPer)
@@ -99,10 +102,13 @@ cy5 <- cos(2*pi*t*5/yearPer)
 # --------------------- MODEL 1 ---------------------------------
 # ---------- regress out weekly, yearly and holidays ------------
 
-mod1 <- arima(dailyInt/100, order=c(1, 1, 3), 
-              seasonal = list(order=c(2, 1, 1), period=7),
-              xreg=cbind(saturdayIx, sundayIx, dailyT))#,
-                        #sy1,cy1,sy2,cy2,sy3,cy3,sy4,cy4,sy5,cy5))
+Acf(diff(dailyInt, 7), lag.max = 100)
+Pacf(diff(dailyInt, 7), lag.max = 100)
+
+mod1 <- arima(dailyInt/100, order=c(1, 0, 3), 
+              seasonal = list(order=c(2, 1, 2), period=7),
+              xreg=cbind(saturdayIx, sundayIx, dailyT, #sw1, cw1, #sw2, cw2,
+                         sy1,cy1,sy2,cy2,sy3,cy3,sy4,cy4,sy5,cy5))
 tsdiag(mod1)
 
 
@@ -111,81 +117,18 @@ qqnorm(mod1$residuals)
 qqline(mod1$residuals)
 
 
-# Only regressors
-# mod1 <- arima(dailyInt, order=c(1, 1, 3),
-#               seasonal = list(order=c(2, 1, 1), period=7),
-#               xreg=cbind(saturdayIx, sundayIx,
-#                          sy1,cy1,sy2,cy2))#,sy3,cy3,sy4,cy4,sy5,cy5))
+# --------------------- MODEL 2 ---------------------------------
+# ------- regress out weekly, yearly and holidays, garch --------
+ressq <- mod1$residuals * mod1$residuals
+Acf(ressq)
+Pacf(ressq)
 
+mod2spec <- ugarchspec(variance.model = list(model = "eGARCH", garchOrder = c(1, 1)),
+                       distribution.model = "std")
+mod2 <- ugarchfit(spec = mod2spec, data = mod1$residuals, solver = "hybrid")
+tsdiag(mod2)
 
+Pacf(mod2@fit$residuals)
+Acf(mod1$residuals)
 
-
-
-
-
-
-
-
-
-# reg1 <- lm(dailyInt ~ freedayIx + 
-#              sin(2*pi*t/weekPer)+cos(2*pi*t/weekPer) +
-#              # sin(2*pi*t*2/weekPer) + cos(2*pi*t*2/weekPer) +
-#              # sin(2*pi*t*3/weekPer) + cos(2*pi*t*3/weekPer) +
-#              sin(2*pi*t/yearPer)+cos(2*pi*t/yearPer) +
-#              sin(2*pi*t*2/yearPer)+cos(2*pi*t*2/yearPer) +
-#              sin(2*pi*t*3/yearPer)+cos(2*pi*t*3/yearPer) +
-#              sin(2*pi*t*4/yearPer)+cos(2*pi*t*4/yearPer) +
-#              sin(2*pi*t*5/yearPer)+cos(2*pi*t*5/yearPer) +
-#              dailyT)
-# 
-# 
-# 
-# res1 <- dailyInt - fitted(reg1)
-# par(mfrow=c(2,1))
-# plot(res1)
-# plot(diff(res1))
-# 
-# par(mfrow=c(3,1))
-# Acf(res1)
-# Acf(diff(res1))
-# Acf(diff(res1, 7))
-# 
-# Pacf(res1)
-# Pacf(diff(res1))
-# Pacf(diff(res1, 7))
-# par(mfrow=c(1,1))
-# 
-# res_decomp = stl(res1, "p", robust = T)
-# plot(res_decomp)
-# 
-# 
-# # --------------------------- sarima -------------------------------------
-# mod1 <- sarima(res1, 1, 1, 3, 2, 1, 1, 7)
-# 
-# 
-# 
-# # --------------------------- model 2 -------------------------------------
-# # treat saturday and sundays seperately
-# # ---------- regress out weekly, yearly and holidays ------------
-# weekPer = 7
-# yearPer = 365
-# t = 1:length(dailyInt)
-# reg2 <- lm(dailyInt ~ saturdayIx + sundayIx + 
-#              sin(2*pi*t/weekPer)+cos(2*pi*t/weekPer) +
-#              # sin(2*pi*t*2/weekPer) + cos(2*pi*t*2/weekPer) +
-#              # sin(2*pi*t*3/weekPer) + cos(2*pi*t*3/weekPer) +
-#              sin(2*pi*t/yearPer)+cos(2*pi*t/yearPer) +
-#              sin(2*pi*t*2/yearPer)+cos(2*pi*t*2/yearPer) +
-#              sin(2*pi*t*3/yearPer)+cos(2*pi*t*3/yearPer) +
-#              sin(2*pi*t*4/yearPer)+cos(2*pi*t*4/yearPer) +
-#              sin(2*pi*t*5/yearPer)+cos(2*pi*t*5/yearPer) +
-#              dailyT)
-# 
-# 
-# 
-# res2 <- dailyInt - fitted(reg2)
-# 
-# plot(res2)
-# plot(diff(res2))
-# Acf(diff(res2))
-# 
+qqnorm(mod2@fit$residuals)
